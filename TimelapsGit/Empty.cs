76d8a9c97aa0f01cd94d48094607ce8278484ec6
Samuel Carlsson <samuel.carlsson@gmail.com>
2012-09-29 01:11:48 +0200

    Adding drag-drop!

---
+++ TimelapsGit/Empty.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimelapsGit
+{
+    public class Empty
+    {
+        public static readonly Empty Value = new Empty();
+    }
+}
--- TimelapsGit/MainWindow.xaml.cs
+++ TimelapsGit/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using GitSharp;
+using System.IO;
+using System.Linq;
 
 namespace TimelapsGit
 {
@@ -13,8 +15,42 @@ namespace TimelapsGit
         {
             InitializeComponent();
 
-            var repo = new Repository(@"F:\src\eliteprospects");
-            DataContext = new MainViewModel(repo, DateTime.Now) { File = @"AndroidManifest.xml" };
+            //var repo = new Repository(@"F:\src\eliteprospects");
+            //DataContext = new MainViewModel(repo, DateTime.Now) { File = @"AndroidManifest.xml" };
+        }
+
+        private void DropList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+            {
+                e.Effects = DragDropEffects.Link;
+            }
+
+            e.Effects = DragDropEffects.None;
+        }
+
+        private void DropList_Drop(object sender, DragEventArgs e)
+        {
+            string[] droppedFilenames = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+
+            var path = droppedFilenames.First();
+            var repo = GetRepository(new DirectoryInfo(Path.GetDirectoryName(path)));
+            DataContext = new MainViewModel(repo, DateTime.Now) { File = Relativize(repo, path) };
+        }
+
+        private string Relativize(Repository repo, string path)
+        {
+            if (path.StartsWith(repo.WorkingDirectory))
+            {
+                return path.Substring(repo.WorkingDirectory.Length).TrimStart("\\".ToArray());
+            }
+            return path;
+        }
+
+        private Repository GetRepository(DirectoryInfo folder)
+        {
+            if (Repository.IsValid(folder.FullName)) return new Repository(folder.FullName);
+            return GetRepository(folder.Parent);
         }
     }
 }
